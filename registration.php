4bd47b033dfbc8cf759a5bb19b5d4bd5980f492d
Rain2o <jrainwater@thinkpyxl.com>
2018-06-08 14:36:29 -0400

    Updated copyright and added license

--- registration.php
+++ registration.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @category    Pyxl
+ * @package     Pyxl_Core
+ * @copyright   © Pyxl, Inc. All rights reserved.
+ * @license     See LICENSE.txt for license details.
+ * @author      Joel Rainwater <jrainwater@pyxl.com>
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
